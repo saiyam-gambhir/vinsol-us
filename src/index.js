ae3316eb53a331bb34a0ba5432aa198eae3cc2d3
Saiyam <saiyam@vinsol.com>
2020-08-28 17:23:05 +0530

    Homepage feedback changes  desktop

--- src/index.js
+++ src/index.js
@@ -11,13 +11,28 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 AOS.init();
 
-$('.wrapper').scroll(function () {
-  AOS.refresh();
-});
-
 import { scripts } from './assets/javascript/script'
 import { scrollBar } from './assets/javascript/scrollBar'
+
 import timeZones from './assets/javascript/timeZones'
+import OurApproach from './assets/javascript/homePage/ourApproach'
+import Navigation from './assets/javascript/navigation'
+
+let ourApproachOptions = {
+  stage: '.stage',
+  window: window
+}
+
+let ourApproach = new OurApproach(ourApproachOptions);
+
+let navigationOptions = {
+  menuBtn: '#menu-toggle-btn',
+  navigationList: '.navigation__list',
+  navigationListItem: '.navigation__list-link'
+}
+
+let navigation = new Navigation(navigationOptions);
+navigation.init();
 
 scripts()
 
@@ -32,13 +47,7 @@ $(window).on('load scroll', function() {
     }, 450)
   }
 
-  $('.stage').each(function() {
-    if(($(window).scrollTop() + $(window).outerHeight()/2) - $(this).outerHeight()/2 > $(this).offset().top) {
-      $(this).addClass('fill-color')
-    } else {
-      $(this).removeClass('fill-color')
-    }
-  })
+  ourApproach.toggleStageColor();
 });
 
 $(document).ready(function() {
@@ -71,9 +80,9 @@ var $status = $('.pagingInfo');
 var $slickElement = $('.slideshow');
 
 $slickElement.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
-    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $status.text(i + '/' + slick.slideCount);
+  //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+  var i = (currentSlide ? currentSlide : 0) + 1;
+  $status.text(i + '/' + slick.slideCount);
 });
 
 $slickElement.slick({
@@ -87,7 +96,7 @@ $slickElement.slick({
 
 $(document).on('keydown', function(e) {
   if(e.keyCode == 13) {
-      $slickElement.slick('slickNext');
+    $slickElement.slick('slickNext');
   }
 });
 
