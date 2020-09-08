47f096751d3c59cfc93f3c4aeb4dd2153494a337
neha188 <neha@vinsol.com>
2020-09-08 13:15:29 +0530

    scrollbar changes

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,5 +1,9 @@
 import '../index.js';
 
+import Scrollbar from 'smooth-scrollbar';
+
+Scrollbar.init(document.querySelector('.map-bg'));
+
 $('.member-name').click(function () {
 	$('.member-list-item').removeClass('show-info');
   $(this).closest('.member-list-item').toggleClass('show-info');
